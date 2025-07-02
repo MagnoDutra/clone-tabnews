3001e004d3ebab58d48e95c51304a78dec04217f
Magno <jmagno.dutra@hotmail.com>
2025-07-02 04:53:14 -0300

    criado testes

---
+++ tests/calculadora.test.js
@@ -0,0 +1,19 @@
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2+2", () => {
+  const result = calculadora.somar(2, 2);
+
+  expect(result).toBe(4);
+});
+
+test("somar 5+100", () => {
+  const result = calculadora.somar(5, 100);
+
+  expect(result).toBe(105);
+});
+
+test("somar banana + 100 deve retornar erro", () => {
+  const result = calculadora.somar("banana", 100);
+
+  expect(result).toBe("Erro");
+});
